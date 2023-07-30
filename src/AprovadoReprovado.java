b3151f18542bbd3d30d4af7dfae907f8cb396533
LucassXDDD <lucasfelipe_17_cn@hotmail.com>
2023-07-30 10:54:02 -0300

    first commit

---
+++ src/AprovadoReprovado.java
@@ -0,0 +1,11 @@
+public class AprovadoReprovado {
+    public static void main(String[] args) {
+        int nota = 8;
+
+        if (nota > 6 ) {
+            System.out.println ("Parabéns, você foi aprovado!");
+        } else {
+            System.out.println("Infelizmente você foi reprovado.");
+        }
+    }
+}
